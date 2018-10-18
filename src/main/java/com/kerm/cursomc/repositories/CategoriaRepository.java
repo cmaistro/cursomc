4e94576be1781f440b9d99c69e3043c70d898bad
carlos <carlos.maistro@gmail.com>
2018-10-17 22:56:47 -0300

    Criado repository e service para Categoria

---
+++ src/main/java/com/kerm/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.kerm.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kerm.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
