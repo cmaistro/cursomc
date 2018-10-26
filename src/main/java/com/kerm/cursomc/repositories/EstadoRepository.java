8eacb6c208513378f778b3a625588bfee4f73545
carlos <carlos.maistro@gmail.com>
2018-10-26 01:52:40 -0300

    Estado e Cidade

---
+++ src/main/java/com/kerm/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.kerm.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kerm.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
